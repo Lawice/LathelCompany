41fa325f39cea4fd0a8bc41e2d8718e5bbcb27a0
lukabartlo <l.bartolo@lahorde.school>
2025-10-27 10:01:14 +0100

    Push Camera

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _playerBody;
+
+    [Header("Camera Settings")]
+    [SerializeField] private float _sensitivityX;
+    [SerializeField] private float _sensitivityY;
+    [SerializeField] private float _clampAngle;
+    
+    private float _xRotation;
+    private float _yRotation;
+
+    private PlayerInputsManager _playerInput;
+    
+    private void Start()
+    {
+        _playerInput = GetComponentInParent<PlayerInputsManager>();
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Looking(_playerInput.LookValue);
+    }
+
+    private void Looking(Vector2 value)
+    {
+        float mouseX = value.x * _sensitivityX;
+        float mouseY = value.y * _sensitivityY;
+
+        _xRotation += mouseX;
+        
+        _yRotation -= mouseY;
+        _yRotation = Mathf.Clamp(_yRotation, -_clampAngle, _clampAngle);
+        
+        transform.rotation = Quaternion.Euler(_yRotation, _xRotation, 0f);
+        
+        _playerBody.rotation = Quaternion.Euler(0f, _xRotation, 0f);
+    }
+}
